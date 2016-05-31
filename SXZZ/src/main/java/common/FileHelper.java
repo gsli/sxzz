2fc0aa3b8a59f75dc70fdbaa4b99c84875e055ff
gaojiangwei <2808996284@qq.com>
2016-05-31 21:36:04 +0800

    搭建环境

---
+++ SXZZ/src/main/java/common/FileHelper.java
@@ -0,0 +1,75 @@
+package common;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public final class FileHelper {
+	public static final String root = "E:\\apache-tomcat-8.0.28\\webapps\\imgs";
+	public static final String filePath = "E:\\apache-tomcat-8.0.28\\webapps\\files";
+	public static String getRealFileName(String fileName){
+        int start = fileName.lastIndexOf("\\");  
+        String value ="";
+        if(start>0){
+        	value = fileName.substring(start+1);
+        }else{
+        	value = fileName;
+        }
+		return value;
+	}
+	
+	public static String generateFileName(String fileName) {
+		DateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+		String formatDate = format.format(new Date());
+		int random = new Random().nextInt(10000);
+		int position = fileName.lastIndexOf(".");
+		String extension = fileName.substring(position);
+		return formatDate + random + extension;
+	}
+
+	public static String FormetFileSize(long fileS) {
+		DecimalFormat df = new DecimalFormat("#.00");
+		String fileSizeString = "";
+		if (fileS < 1024) {
+			fileSizeString = df.format((double) fileS) + "B";
+		} else if (fileS < 1048576) {
+			fileSizeString = df.format((double) fileS / 1024) + "KB";
+		} else if (fileS < 1073741824) {
+			fileSizeString = df.format((double) fileS / 1048576) + "MB";
+		} else {
+			fileSizeString = df.format((double) fileS / 1073741824) + "GB";
+		}
+		return fileSizeString;
+	}
+	
+	public static String upload(File file, String fileName, String root) {
+		InputStream is = null;
+		OutputStream os = null;
+		try { 
+			is = new FileInputStream(file);
+			fileName = FileHelper.generateFileName(fileName);
+	       os = new FileOutputStream(new File(root, fileName));
+	        byte[] buffer = new byte[500];
+	        int length = 0;
+	        
+	        while(-1 != (length = is.read(buffer, 0, buffer.length)))
+	        {
+	            os.write(buffer);
+	        }
+	        os.flush();
+	        os.close();
+	        is.close();
+	        return fileName;
+		} catch (Exception e) {
+			System.out.println("上传失败");
+			return null;
+		}
+	       
+	}
+}
